07f4d7308f3be45b28351928cdc6f3e137415b20
Kazushi Yamanishi <keironia187149@gmail.com>
2019-07-24 10:08:52 +0900

    add column absence? to users

--- db/migrate/20190722110238_create_users.rb
+++ db/migrate/20190722110238_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name, null: false, default: ''
+      t.boolean :absence?, null: false, default: false
       t.timestamps
     end
   end

98d7314c0d3eb3107ef297dbef1bee43899ef6f8
Kazushi Yamanishi <keironia187149@gmail.com>
2019-07-25 13:09:01 +0900

    edit user_entry column

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   has_many :holidays, through: :users_holidays
 
   def self.working(wday)
-    where(absence?: false) - joins(:holidays).where('holidays.id = ?', wday)
+    (all - joins(:holidays).where('holidays.id = ?', wday)) | where(entry?: true)
   end
 
   def self.holiday(wday)
-    joins(:holidays).where('holidays.id = ?', wday) + where(absence?: true)
+    joins(:holidays).where('holidays.id = ?', wday) | where(entry?: false)
   end
 
 end
--- db/migrate/20190722110238_create_users.rb
+++ db/migrate/20190722110238_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name, null: false, default: ''
-      t.boolean :absence?, null: false, default: false
+      t.boolean :entry?
       t.timestamps
     end
   end

33027492ac2894d99b8be37243d15908463569b4
Kazushi Yamanishi <keironia187149@gmail.com>
2019-08-02 10:29:30 +0900

    update seed and csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
 require "csv"
 
 CSV.foreach('db/csv/users.csv', headers: true) do |row|
-  User.find_or_create_by(
-    id: row['id'],
-    name: row['name']
-  )
+  User.where(name: row['name']).first_or_create do |user|
+    user.id = row['id']
+  end
 end
 
 CSV.foreach('db/csv/holidays.csv', headers: true) do |row|
@@ -14,6 +13,7 @@ CSV.foreach('db/csv/holidays.csv', headers: true) do |row|
   )
 end
 
+UsersHoliday.destroy_all
 CSV.foreach('db/csv/users_holidays.csv', headers: true) do |row|
   UsersHoliday.create!(
     id: row['id'],

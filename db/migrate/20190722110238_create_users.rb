fbf3dfd59351cc8fa21d693adc08a6fce714e413
Kazushi Yamanishi <keironia187149@gmail.com>
2020-01-27 00:39:06 +0900

    modify users and holidays table

--- db/migrate/20190722110238_create_users.rb
+++ db/migrate/20190722110238_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name, null: false, default: ''
+      t.integer :job, null: false, default: 0
       t.boolean :entry?
       t.timestamps
     end
--- db/migrate/20190722114103_create_holidays.rb
+++ db/migrate/20190722114103_create_holidays.rb
@@ -1,8 +1,8 @@
 class CreateHolidays < ActiveRecord::Migration[5.2]
   def change
     create_table :holidays do |t|
-      t.string :name, null: false, default: ''
-
+      t.integer :wday, null: false, default: 0
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end

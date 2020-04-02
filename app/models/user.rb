87d7361a5335faaab088b6aa64c94c372bd07ac5
Kazushi Yamanishi <keironia187149@gmail.com>
2020-04-02 19:17:12 +0900

    update users csv 20200401

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_many :holidays
 
-  enum job: { holiday: 0, tc_sales: 1, life_coach: 2, mentor: 3, carrier: 4, exp_sales: 5, all_day: 6 }
+  enum job: { holiday: 0, tc_sales: 1, life_coach: 2, mentor: 3, carrier: 4, invisible: 5, all_day: 6 }
 
   def self.working(wday)
 
     # holiday, salesで無い→休日がwday出ない→もしくはentry?がtrue の人が基本出席
-    base = (where(entry?: nil).where.not(job: [ :holiday, :tc_sales, :exp_sales]) - joins(:holidays)
+    base = (where(entry?: nil).where.not(job: [ :holiday, :tc_sales, :invisible]) - joins(:holidays)
     .where('holidays.wday = ?', wday)) | where(entry?: true)
     case wday
     when 1, 4
@@ -26,7 +26,7 @@ class User < ApplicationRecord
 
   def self.holiday(wday)
     # expセールス(非表示)以外から出社組を引く→もしくは欠席押してる人が欠席
-    where.not(job: [:exp_sales]) - working(wday) | where(entry?: false)
+    where.not(job: [:invisible]) - working(wday) | where(entry?: false)
   end
 
 end
